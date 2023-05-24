0341885cc3c19cfc38a3dc8680714b5368584a9a
waqar.ahmad <waqar.ahmad@gotmotive.com>
2023-05-25 02:22:17 +0500

    Understanding withContext

--- app/src/main/java/com/waqar/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/waqar/kotlincoroutines/MainActivity.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
@@ -22,116 +23,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         counterText = findViewById(R.id.counter_text)
 
-        GlobalScope.launch {
-            executeTask()
+
+        // until coroutines are not complete runBlocking will block thread
+        runBlocking {
+           launch {
+                delay(1000)
+                Log.d(TAG, "world")
+            }
+            Log.d(TAG, "hello")
         }
     }
 
-    private suspend fun executeLonRunningTask(){
-    for(i in 1..10000000){
 
-    }
-    }
 
     private suspend fun executeTask() {
-
-
-val parentJob = CoroutineScope(Dispatchers.IO).launch {
-    for (i in 1..1000){
-        if (this.isActive) {
-            executeLonRunningTask()
-            Log.d(TAG, i.toString())
+        Log.d(TAG, "Before")
+        // With context is of blocking nature and block coroutine to finish first
+        withContext(Dispatchers.IO) {
+            delay(1000)
+            Log.d(TAG, "Inside")
         }
-    }
-}
-
-        delay(100)
-        Log.d(TAG, "Cancelling Job")
-        parentJob.cancel()
-        parentJob.join()
-        Log.d(TAG, "Parent completed")
-
-
-
-//        val parentJob = GlobalScope.launch(Dispatchers.Main) {
-//            Log.d(TAG, "Parent Started")
-//
-//            val childJob = launch {
-//                try {
-//                    Log.d(TAG, "Child Started")
-//                    delay(5000)
-//                    Log.d(TAG, "Child Ended")
-//                }catch (e:CancellationException){
-//                    Log.d(TAG, "Child Canceled")
-//                }
-//            }
-//
-//            delay(3000)
-//            childJob.cancel()
-//            Log.d(TAG, "Parent Ended")
-//        }
-
-//        delay(1000)
-
-        // Parent job cancel will cancel child jobs
-//        parentJob.cancel()
-
-        // Parent job delayed until all child jobs completed
-//        parentJob.join()
-//        Log.d(TAG, "Parent Completed")
-
-//        Log.d(TAG, "before")
-//        withContext(Dispatchers.IO) {
-//            delay(1000)
-//            Log.d(TAG, "inside")
-//        }
-//        Log.d(TAG, "After")
+        Log.d(TAG, "After")
     }
 
-//    private suspend fun printFollowers() {
-//        // Using Launch
-//        var fbFollowers = 0
-//        var instagramFollowers = 0
-//        val job1 = CoroutineScope(Dispatchers.IO).launch {
-//            fbFollowers = getFbFollowers()
-//        }
-//
-//        val job2 = CoroutineScope(Dispatchers.IO).launch {
-//            instagramFollowers = getInstagramFollowers()
-//        }
-//
-//        job1.join()
-//        job2.join()
-//        Log.d(TAG, "FB - $fbFollowers, Insta $instagramFollowers.toString()")
-
-        // Using Async
-//        val fb = CoroutineScope(Dispatchers.IO).async {
-//            getFbFollowers()
-//        }
-//
-//        val instagram = CoroutineScope(Dispatchers.IO).async {
-//             getInstagramFollowers()
-//        }
-//
-//        Log.d(TAG, "FB - ${fb.await()}, Instagram ${instagram.await()}.toString()")
-//
-//        // Another way to do it
-//        CoroutineScope(Dispatchers.IO).launch {
-//            val fb = async { getFbFollowers() }
-//            val instagram = async { getInstagramFollowers() }
-//            Log.d(TAG, "FB - ${fb.await()}, Instagram ${instagram.await()}.toString()")
-//        }
-//
-//    }
-//
-//    private suspend fun getFbFollowers():Int {
-//        delay(1000)
-//        return 54
-//    }
-//
-//    private suspend fun getInstagramFollowers():Int {
-//        delay(1000)
-//        return 113
-//    }
 
 }
